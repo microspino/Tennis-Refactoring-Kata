3fe5c1350abb9a52beffa948a06c12ed23191d74
Daniele Spinosa <info@microspino.com>
2022-08-04 20:48:07 +0200

    Enable all tests

--- ruby/tennis.rb
+++ ruby/tennis.rb
@@ -7,37 +7,37 @@ Player = Struct.new(:name, :points) do
   def wordy_points; WORDY_PTS[points]; end
 end
 
-# class TennisGame1
-#   def initialize(a_name, b_name)
-#     @a = Player.new(a_name, 0)
-#     @b = Player.new(b_name, 0)
-#   end
-#
-#   def won_point(name)
-#     name == 'player1' ? @a.points += 1 : @b.points += 1
-#   end
-#
-#   def advantage_or_win(score_diff)
-#     case score_diff
-#     when 1
-#       'Advantage player1'
-#     when -1
-#       'Advantage player2'
-#     when (2..)
-#       'Win for player1'
-#     else
-#       'Win for player2'
-#     end
-#   end
-#
-#   def score
-#     return WORDY_ALL[@a.points] || "Deuce" if @a.points == @b.points
-#
-#     return "#{@a.wordy_points}-#{@b.wordy_points}" if @a.points < 4 && @b.points < 4
-#
-#     advantage_or_win(@a.points - @b.points)
-#   end
-# end
+class TennisGame1
+  def initialize(a_name, b_name)
+    @a = Player.new(a_name, 0)
+    @b = Player.new(b_name, 0)
+  end
+
+  def won_point(name)
+    name == 'player1' ? @a.points += 1 : @b.points += 1
+  end
+
+  def advantage_or_win(score_diff)
+    case score_diff
+    when 1
+      'Advantage player1'
+    when -1
+      'Advantage player2'
+    when (2..)
+      'Win for player1'
+    else
+      'Win for player2'
+    end
+  end
+
+  def score
+    return WORDY_ALL[@a.points] || "Deuce" if @a.points == @b.points
+
+    return "#{@a.wordy_points}-#{@b.wordy_points}" if @a.points < 4 && @b.points < 4
+
+    advantage_or_win(@a.points - @b.points)
+  end
+end
 
 class TennisGame2
   def initialize(a_name, b_name)
@@ -48,12 +48,16 @@ class TennisGame2
   def won_point(name)
     name == @a.name ? @a.points +=1 : @b.points +=1
   end
+  
+  def tie?
+    @a.points == @b.points
+  end
 
   def score
-    return "Deuce" if @a.points == @b.points && @a.points > 2
+    return "Deuce" if tie? && @a.points > 2
 
     w = ["Love", "Fifteen", "Thirty", "Forty"]
-    return w[@a.points] + "-All" if @a.points == @b.points && @a.points < 3
+    return w[@a.points] + "-All" if tie? && @a.points < 3
     
     diff = @a.points - @b.points
 
@@ -71,34 +75,34 @@ class TennisGame2
   end
 end
 
-# class TennisGame3
-#   def initialize(a_name, b_name)
-#     @a = Player.new(a_name, 0)
-#     @b = Player.new(b_name, 0)
-#   end
-#
-#   def won_point(player_name)
-#     player_name == @a.name ? @a.points += 1 : @b.points += 1
-#   end
-#
-#   def advantage?
-#     diff =  @a.points - @b.points
-#     diff * diff == 1
-#   end
-#
-#   def tie?
-#     @a.points == @b.points
-#   end
-#
-#   def score
-#     return "Deuce" if tie? && @a.points >= 3
-#     return "#{@a.wordy_points}-All" if tie? && @a.points < 4
-#
-#     if @a.points < 4 && @b.points < 4
-#       "#{@a.wordy_points}-#{@b.wordy_points}"
-#     else
-#       x = [@a, @b].max_by(&:points)
-#       advantage? ? "Advantage #{x.name}" : "Win for #{x.name}"
-#     end
-#   end
-# end
+class TennisGame3
+  def initialize(a_name, b_name)
+    @a = Player.new(a_name, 0)
+    @b = Player.new(b_name, 0)
+  end
+
+  def won_point(player_name)
+    player_name == @a.name ? @a.points += 1 : @b.points += 1
+  end
+
+  def advantage?
+    diff =  @a.points - @b.points
+    diff * diff == 1
+  end
+
+  def tie?
+    @a.points == @b.points
+  end
+
+  def score
+    return "Deuce" if tie? && @a.points >= 3
+    return "#{@a.wordy_points}-All" if tie? && @a.points < 4
+
+    if @a.points < 4 && @b.points < 4
+      "#{@a.wordy_points}-#{@b.wordy_points}"
+    else
+      x = [@a, @b].max_by(&:points)
+      advantage? ? "Advantage #{x.name}" : "Win for #{x.name}"
+    end
+  end
+end
